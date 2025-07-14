3dff33d235437d828849f5bced6c97de7ba30689
Cao Hữu Huy <huy45623@gmail.com>
2025-07-14 11:37:05 +0700

    up

---
+++ src/main/java/com/lifesup/bizstore/BizStoreApplication.java
@@ -0,0 +1,13 @@
+package com.lifesup.bizstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifesup/bizstore/BizStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifesup.bizstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
